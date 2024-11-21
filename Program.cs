763812953838a223653a936acdcc89edadced52c
Edvin Runhellen <edvin.runhellen1@gmail.com>
2024-11-21 15:42:22 +0100

    First version

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Guessinggame;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Guess the secret word within 5 guesses or you lose!");
+        string secretWord = "github";
+        string guess = "";
+        int guessCount = 0;
+        int guessLimit = 5;
+        bool outOfGuesses = false;
+        int guessesLeft = 5; 
+        
+        while (guess != secretWord && !outOfGuesses)
+        {
+            if (guessCount < guessLimit)
+            {
+                Console.Write($"{guessesLeft} guesses left .. Enter guess: ");
+                guess = Console.ReadLine();
+                guessCount++;
+                guessesLeft--;
+            }
+            else
+            {
+                outOfGuesses = true;
+            }
+        }
+
+        if (outOfGuesses)
+        {
+            Console.Write("You lose!");
+        }
+        else
+        {
+            Console.Write("You guessed it!");
+        }
+    }
+}
